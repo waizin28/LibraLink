de5ceaec9cc51647f4923b4a12e877e57d712325
waizin28 <waizinlinn.08@gmail.com>
2024-12-28 22:17:29 -0600

    created ui for admin privledges

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -12,6 +12,7 @@ import LoginWidget from './Auth/LoginWidget';
 import ReviewListPage from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import ShelfPage from './layouts/ShelfPage/ShelfPage';
 import MessagesPage from './layouts/MessagesPage/MessagesPage';
+import ManageLibraryPage from './layouts/ManageLibraryPage/ManageLibraryPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -63,6 +64,10 @@ export const App = () => {
 
             <SecureRoute path='/messages'>
               <MessagesPage />
+            </SecureRoute>
+
+            <SecureRoute path='/admin'>
+              <ManageLibraryPage />
             </SecureRoute>
           </Switch>
         </div>
