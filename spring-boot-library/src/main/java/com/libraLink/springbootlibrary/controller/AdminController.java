66adea4846061f494c9119564070d699d1906017
waizin28 <waizinlinn.08@gmail.com>
2024-12-29 14:08:49 -0600

    added api end point to add new book

---
+++ spring-boot-library/src/main/java/com/libraLink/springbootlibrary/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.libraLink.springbootlibrary.controller;
+
+import com.libraLink.springbootlibrary.requestmodels.AddBookRequest;
+import com.libraLink.springbootlibrary.service.AdminService;
+import com.libraLink.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value = "Authorization") String token, @RequestBody AddBookRequest addBookRequest) throws Exception{
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if(admin == null || !admin.equals("admin")){
+            throw new Exception("Administration access require");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
