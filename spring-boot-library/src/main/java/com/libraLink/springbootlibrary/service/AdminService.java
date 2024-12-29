3ea23af89615b75eb0f856d204f8041d0e539811
waizin28 <waizinlinn.08@gmail.com>
2024-12-29 14:04:52 -0600

    added service method to add new book

---
+++ spring-boot-library/src/main/java/com/libraLink/springbootlibrary/service/AdminService.java
@@ -0,0 +1,32 @@
+package com.libraLink.springbootlibrary.service;
+
+import com.libraLink.springbootlibrary.entity.Book;
+import com.libraLink.springbootlibrary.repository.BookRepository;
+import com.libraLink.springbootlibrary.requestmodels.AddBookRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AdminService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public AdminService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void postBook(AddBookRequest addBookRequest){
+        Book book = new Book();
+        book.setTitle(addBookRequest.getTitle());
+        book.setAuthor(addBookRequest.getAuthor());
+        book.setDescription(addBookRequest.getDescription());
+        book.setCopies(addBookRequest.getCopies());
+        book.setCopiesAvailable(addBookRequest.getCopies());
+        book.setCategory(addBookRequest.getCategory());
+        book.setImg(addBookRequest.getImg());
+        bookRepository.save(book);
+    }
+}
